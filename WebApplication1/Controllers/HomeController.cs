3f94e6099ede0ab552fa73906fce688e115424ed
Mateus Pereira <mateusmartinspereira@gmail.com>
2020-08-21 14:48:05 -0300

    Model-Controller-View Department
    Built for Department:
    - Model
    - Controller
    - View

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Models.ViewModels;
 
 namespace WebApplication1.Controllers
 {

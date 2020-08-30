41e5f6e7721ab89b9182db2fd0cbe330abcb9830
Mateus Pereira <mateusmartinspereira@gmail.com>
2020-08-29 22:45:23 -0300

    Other entities and second migration

--- WebApplication1/Migrations/20200830014216_OtherEntities.Designer.cs
+++ WebApplication1/Migrations/20200830014216_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication1.Data;
 
 namespace WebApplication1.Migrations
 {
     [DbContext(typeof(swmContext))]
-    partial class swmContextModelSnapshot : ModelSnapshot
+    [Migration("20200830014216_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

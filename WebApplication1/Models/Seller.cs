8afdb99c43ba98d84290fc3699a6a4a49fc092ce
Mateus Pereira <mateusmartinspereira@gmail.com>
2020-09-20 14:03:31 -0300

    Foreign key not null (referential integrity)

--- WebApplication1/Models/Seller.cs
+++ WebApplication1/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace WebApplication1.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
         public Seller()
